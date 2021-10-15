1fb5b139c94b51e010502606036973002f120608
Bruno Lopes <eusoubruno7@gmail.com>
2021-10-15 14:46:04 -0300

    fix fixing accents and renaming variables

--- compactaDescompacta.h
+++ compactaDescompacta.h
@@ -7,168 +7,154 @@
 #include <bitset>
 #include "heap.h"
 #include "tree.h"
-nodo CreateTreeCompact(string arquivo)
-{
+
+nodo CreateTreeCompact(string arquivo){
   int vet[256];
-  for (int i = 0; i < 256; i++)
-  {
+
+  for (int i = 0; i < 256; i++){
     vet[i] = 0;
   }
+
   int ichar;
-  char a;
-  unsigned char b;
-  int teste = 0;
+  char aux;
+  unsigned char charAux;
+
   ifstream myfile(arquivo.c_str(), ifstream::binary);
-  myfile.read(&a, 1);
-  while (!myfile.eof())
-  {
-    b = a;
-    ichar = (int)b;
-    for (int i = 0; i < 256; i++)
-    {
-      if (ichar == i)
-      {
+  myfile.read(&aux, 1);
+
+  while (!myfile.eof()){
+    charAux = aux;
+    ichar = (int)charAux;
+    for (int i = 0; i < 256; i++){
+      if (ichar == i){
         vet[i]++;
       }
     }
-    myfile.read(&a, 1);
+    myfile.read(&aux, 1);
   }
+
   myfile.close();
+
   int tamHeap = 0, cont = 0;
-  for (int i = 0; i < 256; i++)
-  {
-    if (vet[i] != 0)
-    {
+  for (int i = 0; i < 256; i++){
+    if (vet[i] != 0){
       tamHeap++;
     }
   }
   stack<nodo> pilha;
-  for (int i = 0; i < 256; i++)
-  {
-    if (vet[i] != 0)
-    {
-      nodo a(vet[i], i);
-      pilha.push(a);
+  for (int i = 0; i < 256; i++){
+    if (vet[i] != 0){
+      nodo no(vet[i], i);
+      pilha.push(no);
       cont++;
     }
   }
-  heap<nodo> h(cont);
-  for (int i = 0; i < cont; i++)
-  {
-    h.push(pilha.top());
+  
+  heap<nodo> heapAux(cont);
+  for (int i = 0; i < cont; i++){
+    heapAux.push(pilha.top());
     pilha.pop();
   }
-  nodo l, r;
-  while (h.len() > 1)
-  {
-    l = h.pop();
-    r = h.pop();
-    nodo c(&l, &r);
-    h.push(c);
+
+  nodo lAux, rAux;
+  while (heapAux.len() > 1){
+    lAux = heapAux.pop();
+    rAux = heapAux.pop();
+    nodo no(&lAux, &rAux);
+    heapAux.push(no);
   }
-  r = h.pop();
-  nodo retorno = new nodo(r);
+
+  rAux = heapAux.pop();
+  nodo retorno = new nodo(rAux);
+
   return retorno;
 }
-nodo *CreateTreeDescompact(string arquivo)
-{
+
+
+nodo *CreateTreeDescompact(string arquivo){
+
   int contStop = 0, contBarras = 0, contPontoVirgulas = 0;
-  char a;
-  unsigned char b, caracAnterior, posTabela;
+  char aux;
+  unsigned char bAux, caracAnterior, posTabela;
   string vetor[256], sequence = "";
-  for (int i = 0; i < 256; i++)
-  {
+
+  for (int i = 0; i < 256; i++){
     vetor[i] = "";
   }
+
   nodo *retorno = new nodo();
   ifstream myfile(arquivo.c_str(), ifstream::binary);
-  myfile.read(&a, 1);
-  while (!myfile.eof())
-  {
-    b = a;
-    if (contBarras > 1)
-    {
-      if (caracAnterior == '/')
-      {
-        posTabela = b;
-        myfile.read(&a, 1);
-        myfile.read(&a, 1);
-        caracAnterior = a;
+  myfile.read(&aux, 1);
+
+  while (!myfile.eof()){
+    bAux = aux;
+
+    if (contBarras > 1){
+      if (caracAnterior == '/'){
+        posTabela = bAux;
+        myfile.read(&aux, 1);
+        myfile.read(&aux, 1);
+        caracAnterior = aux;
         continue;
       }
-      else
-      {
-        if (b != '/' || b != '|')
-        {
-          if (b == '|')
-          {
+      else{
+        if (bAux != '/' || bAux != '|'){
+          if (bAux == '|'){
             contStop++;
           }
-          else if (b == '/')
-          {
+          else if (bAux == '/'){
             vetor[(int)posTabela] = sequence;
             sequence = "";
           }
-          else
-          {
-            sequence += b;
+          else{
+            sequence += bAux;
           }
-        }
-        else
-        {
         }
       }
     }
-    if (b == '/')
-    {
+
+    if (bAux == '/'){
       contBarras++;
     }
-    if (contStop == 3)
-    {
+    if (contStop == 3){
       vetor[(int)posTabela] = sequence;
       break;
     }
-    caracAnterior = a;
-    myfile.read(&a, 1);
+    caracAnterior = aux;
+    myfile.read(&aux, 1);
   }
-  for (int i = 0; i < 256; i++)
-  {
+
+  for (int i = 0; i < 256; i++){
     nodo *aux = retorno;
     sequence = vetor[i];
-    for (int j = 0; j < sequence.length(); j++)
-    {
-      if (sequence[j] == '0')
-      {
-        if (aux->left == 0)
-        {
+
+    for (int j = 0; j < sequence.length(); j++){
+      if (sequence[j] == '0'){
+        if (aux->left == 0){
           nodo *temp = new nodo();
           aux->left = temp;
           aux = aux->left;
         }
-        else
-        {
+        else{
           aux = aux->left;
         }
       }
-      else
-      {
-        if (aux->right == 0)
-        {
+      else{
+        if (aux->right == 0){
           nodo *temp = new nodo();
           aux->right = temp;
           aux = aux->right;
         }
-        else
-        {
+        else{
           aux = aux->right;
         }
       }
-      if (j + 1 == sequence.length())
-      {
+      if (j + 1 == sequence.length()){
         aux->caractere = i;
       }
     }
   }
+
   myfile.close();
   return retorno;
 }
@@ -185,101 +171,103 @@ void codigoCarac(nodo *no, string sequence, string vet[])
     codigoCarac(no->right, sequence + '1', vet);
   }
 }
-string retornaArqSaida(string nomeArquivo)
-{
+
+
+
+string retornaArqSaida(string nomeArquivo){
   string newName = "";
-  for (int j = 0; j < nomeArquivo.length(); j++)
-  {
-    if (nomeArquivo[j] == '.')
-    {
+  for (int j = 0; j < nomeArquivo.length(); j++){
+    if (nomeArquivo[j] == '.'){
       break;
     }
     newName += nomeArquivo[j];
   }
+
   newName += ".bruno";
   return newName;
 }
-string retornaExtensaoArq(string nomeArquivo)
-{
+
+string retornaExtensaoArq(string nomeArquivo){
+
   string ext = "";
   int pos;
-  for (int j = 0; j < nomeArquivo.length(); j++)
-  {
-    if (nomeArquivo[j] == '.')
-    {
+
+  for (int j = 0; j < nomeArquivo.length(); j++){
+    if (nomeArquivo[j] == '.'){
       pos = j;
     }
   }
-  for (int j = pos + 1; j < nomeArquivo.length(); j++)
-  {
+
+  for (int j = pos + 1; j < nomeArquivo.length(); j++){
     ext += nomeArquivo[j];
   }
+
   return ext;
 }
-string retornaNome(string nomeArquivo)
-{
+
+
+string retornaNome(string nomeArquivo){
   string nome = "";
   int pos;
-  for (int j = 0; j < nomeArquivo.length(); j++)
-  {
-    if (nomeArquivo[j] == '.')
-    {
+
+  for (int j = 0; j < nomeArquivo.length(); j++){
+    if (nomeArquivo[j] == '.'){
       pos = j;
     }
   }
-  for (int j = 0; j < pos; j++)
-  {
+  for (int j = 0; j < pos; j++){
     nome += nomeArquivo[j];
   }
+
   nome += ".";
-  char a;
-  unsigned char b;
+  char aux;
+  unsigned char bAux;
   int contBarras = 0;
   ifstream myfile(nomeArquivo.c_str(), ifstream::binary);
-  myfile.read(&a, 1);
-  while (!myfile.eof())
-  {
-    b = a;
-    if (contBarras > 0)
-    {
+  myfile.read(&aux, 1);
 
-      if (b == '/')
-      {
+  while (!myfile.eof()){
+    bAux = aux;
+    if (contBarras > 0){
+      if (bAux == '/'){
         break;
       }
-
-      nome += b;
+      nome += bAux;
     }
-    if (b == '/')
-    {
+
+    if (bAux == '/'){
       contBarras++;
     }
-    myfile.read(&a, 1);
+
+    myfile.read(&aux, 1);
   }
+
   myfile.close();
   return nome;
 }
-int quantidadePosComValor(string vet[])
-{
+
+
+int quantidadePosComValor(string vet[]){
   int cont = 0;
-  for (int i = 0; i < 256; i++)
-  {
-    if (vet[i] != "")
-    {
+
+  for (int i = 0; i < 256; i++){
+    if (vet[i] != ""){
       cont++;
     }
   }
+
   return cont;
 }
-void compactar(string nomeArquivo)
-{
+void compactar(string nomeArquivo){
   ifstream myFileInput(nomeArquivo.c_str(), ifstream::binary);
-  if (!myFileInput)
-  {
+
+  if (!myFileInput){
     cout << "Arquivo Inexistente!!" << endl;
     return;
   }
+
   cout << "Compactando o arquivo..." << endl;
+
   int soma = 0, cont = 0, tamTabela;
   tree *arvore = new tree(CreateTreeCompact(nomeArquivo));
   string sequence = "", by = "";
@@ -289,184 +277,179 @@ void compactar(string nomeArquivo)
   myFileOutput << arvore->getNodoRaiz()->freq << '/';
   myFileOutput << retornaExtensaoArq(nomeArquivo) << '/';
   tamTabela = quantidadePosComValor(vetor);
-  for (int i = 0; i < 256; i++)
-  {
-    if (vetor[i] != "")
-    {
+
+  for (int i = 0; i < 256; i++){
+    if (vetor[i] != ""){
       cont++;
       myFileOutput << (char)i << ';';
       by = vetor[i];
-      for (int j = 0; j < by.length(); j++)
-      {
+      for (int j = 0; j < by.length(); j++){
         myFileOutput << by[j];
       }
-      if (cont == tamTabela)
-      {
+      if (cont == tamTabela){
         myFileOutput << '|' << '|' << '|';
       }
-      else
-      {
+      else{
         myFileOutput << '/';
       }
     }
   }
-  char a;
-  unsigned char b;
+
+  char aux;
+  unsigned char bAux;
   queue<char> fila;
   cont = 0;
-  myFileInput.read(&a, 1);
-  while (!myFileInput.eof())
-  {
-    b = a;
+  myFileInput.read(&aux, 1);
+
+  while (!myFileInput.eof()){
+    bAux = aux;
     sequence = "";
-    for (int i = 0; i < 256; i++)
-    {
-      if (b == i)
-      {
+
+    for (int i = 0; i < 256; i++){
+      if (bAux == i){
         cont++;
         sequence = vetor[i];
-        for (int j = 0; j < sequence.length(); j++)
-        {
+
+        for (int j = 0; j < sequence.length(); j++){
           fila.push(sequence[j]);
         }
       }
     }
-    while (fila.size() > 7)
-    {
+
+    while (fila.size() > 7){
       sequence = "";
-      for (int i = 0; i < 8; i++)
-      {
+      for (int i = 0; i < 8; i++){
         sequence += fila.front();
         fila.pop();
       }
-      for (int i = 0, j = sequence.length() - 1; i < sequence.length(); i++, j--)
-      {
-        if (sequence[j] == '1')
-        {
+      for (int i = 0, j = sequence.length() - 1; i < sequence.length(); i++, j--){
+        if (sequence[j] == '1'){
           soma += pow(2, i);
         }
       }
+
       myFileOutput << (char)soma;
       soma = 0;
     }
-    myFileInput.read(&a, 1);
+
+    myFileInput.read(&aux, 1);
   }
+
   int tamFila;
-  if (fila.size() < 8)
-  {
+
+  if (fila.size() < 8){
     tamFila = fila.size();
     sequence = "";
-    while (!fila.empty())
-    {
+
+    while (!fila.empty()){
       sequence += fila.front();
       fila.pop();
     }
-    for (int i = tamFila; i < 8; i++)
-    {
+    for (int i = tamFila; i < 8; i++){
       sequence += '0';
     }
-    for (int i = 0, j = sequence.length() - 1; i < sequence.length(); i++, j--)
-    {
-      if (sequence[j] == '1')
-      {
+
+    for (int i = 0, j = sequence.length() - 1; i < sequence.length(); i++, j--){
+      if (sequence[j] == '1'){
         soma += pow(2, i);
       }
     }
+
     myFileOutput << (char)soma;
   }
+
   myFileOutput.close();
   myFileInput.close();
+
   cout << "concluido." << endl;
 }
-void descompactar(string nomeArquivo)
-{
+
+
+void descompactar(string nomeArquivo){
   ifstream myfileInput(nomeArquivo.c_str(), ifstream::binary);
-  if (!myfileInput)
-  {
+
+  if (!myfileInput){
     cout << "Arquivo Inexistente!!" << endl;
     return;
   }
+
   cout << "Descompactando o arquivo..." << endl;
+
   tree *arvore = new tree(CreateTreeDescompact(nomeArquivo));
   string nomeDescompact = retornaNome(nomeArquivo), sequence = "";
-  char a;
+  char aux;
   bool achouCarac = false;
-  unsigned char b;
-  nodo *aux;
+  unsigned char bAux;
+  nodo *no;
   int quantCaracteres, contCarac = 0, fimTable = 0;
   queue<char> fila;
   ofstream myfileOutput(nomeDescompact.c_str(), ofstream::binary);
-  myfileInput.read(&a, 1);
-  while (!myfileInput.eof())
-  {
-    if (a == '/')
-    {
+  myfileInput.read(&aux, 1);
+  while (!myfileInput.eof()){
+    if (aux == '/'){
       break;
     }
-    b = a;
-    sequence += b;
-    myfileInput.read(&a, 1);
+    bAux = aux;
+    sequence += bAux;
+    myfileInput.read(&aux, 1);
   }
-  myfileInput.read(&a, 1);
-  while (!myfileInput.eof())
-  {
-    if (a == '|')
-    {
-      myfileInput.read(&a, 1);
-      if (a == '|')
-      {
-        myfileInput.read(&a, 1);
-        if (a == '|')
+  myfileInput.read(&aux, 1);
+  while (!myfileInput.eof()){
+    if (aux == '|'){
+      myfileInput.read(&aux, 1);
+
+      if (aux == '|'){
+        myfileInput.read(&aux, 1);
+        if (aux == '|')
         {
           break;
         }
       }
     }
-    myfileInput.read(&a, 1);
+
+    myfileInput.read(&aux, 1);
   }
   quantCaracteres = stoi(sequence);
-  myfileInput.read(&a, 1);
-  aux = arvore->getNodoRaiz();
-  while (!myfileInput.eof())
-  {
-    b = a;
-    bitset<8> bs3((int)b);
+  myfileInput.read(&aux, 1);
+  no = arvore->getNodoRaiz();
+
+  while (!myfileInput.eof()){
+    bAux = aux;
+    bitset<8> bs3((int)bAux);
     sequence = bs3.to_string();
-    for (int j = 0; j < sequence.length(); j++)
-    {
+    for (int j = 0; j < sequence.length(); j++){
       fila.push(sequence[j]);
     }
-    while (!fila.empty())
-    {
-      if (contCarac == quantCaracteres)
-      {
+
+    while (!fila.empty()){
+      if (contCarac == quantCaracteres){
         break;
       }
-      if (fila.front() == '0')
-      {
+      if (fila.front() == '0'){
         fila.pop();
-        aux = aux->left;
-        if (aux->left == 0 && aux->right == 0)
-        {
-          myfileOutput << aux->caractere;
-          aux = arvore->getNodoRaiz();
+
+        no = no->left;
+        if (no->left == 0 && no->right == 0){
+          myfileOutput << no->caractere;
+          no = arvore->getNodoRaiz();
           contCarac++;
         }
       }
-      else if (fila.front() == '1')
-      {
+      else if (fila.front() == '1'){
         fila.pop();
-        aux = aux->right;
-        if (aux->left == 0 && aux->right == 0)
-        {
-          myfileOutput << aux->caractere;
-          aux = arvore->getNodoRaiz();
+        no = no->right;
+
+        if (no->left == 0 && no->right == 0){
+
+          myfileOutput << no->caractere;
+          no = arvore->getNodoRaiz();
           contCarac++;
         }
       }
     }
-    myfileInput.read(&a, 1);
+    myfileInput.read(&aux, 1);
   }
+
   myfileOutput.close();
   myfileInput.close();
   cout << "Concluido." << endl;
--- heap.h
+++ heap.h
@@ -1,43 +1,40 @@
 #include <iostream>
+
 using namespace std;
+
 template <class T>
-class heap
-{
+
+class heap{
   T *data;
   int n, m;
-  void swap(int i, int j)
-  {
+  void swap(int i, int j){
     T x = data[i];
     data[i] = data[j];
     data[j] = x;
   }
-  void _subir_(int i)
-  {
+
+  void _subir_(int i){
     int j = (i - 1) / 2;
-    if (i == 0)
-      return;
-    if (data[j] > data[i])
-    {
+    if (i == 0) return;
+
+    if (data[j] > data[i]){
       swap(i, j);
       _subir_(j);
     }
   }
-  void _descer_(int i)
-  {
+
+  void _descer_(int i){
     int j = 2 * i + 1;
-    if (j > m - 1)
-      return;
-    if (j == m - 1)
-    {
-      if (data[i] > data[j])
-        swap(i, j);
+    if (j > m - 1) return;
+
+    if (j == m - 1){
+      if (data[i] > data[j]) swap(i, j);
       _descer_(j);
     }
-    else
-    {
+    else{
       int p = data[j] < data[j + 1] ? j : j + 1;
-      if (data[i] > data[p])
-      {
+      
+      if (data[i] > data[p]){
         swap(i, p);
         _descer_(p);
       }
